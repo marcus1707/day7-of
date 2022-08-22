5e61008162c286f83737ab0990b8df7ce97c3444
marcus <marcus@kali.marcus>
2022-08-22 05:38:53 -0400

    here we go

---
+++ js/main.js
@@ -0,0 +1,118 @@
+function greet(name) {      // decloration 1
+    console.log("Hello " + name + "");
+}
+
+greet('Marcus');
+
+
+
+// function sum (num1, num2) {  // decloration 2
+//     console.log(num1 + num2);
+// }
+
+// sum(20, 85);
+
+
+
+// function trend(trend1, trend2) {    // decloration 3
+//     console.log("O'zbekistonda bu yilgi trend qo'shiqlar:" + trend1 + " va " + trend2);
+// }
+
+// trend("Dunyo seni tog'angmas", "Yalala bobo");
+
+
+
+// let cost1 = " 65 mln. dollor";    // decloration 4
+//     club1 = " from Real Madrid";
+
+//     cost2 = " 60 mln. dollor";
+//     club2 = " from Ajax";
+
+//     cost3 = " 0";
+//     club3 = " as free agent";
+
+// function transfer(transfer1, transfer2, transfer3) {
+//     console.log(transfer1 + cost1 + club1);
+//     console.log(transfer2 + cost2 + club2);
+//     console.log(transfer3 + cost3 + club3);    
+// }
+
+// transfer("Casemiro", "Martinez", "Eriksen");
+
+
+
+// function myWorld() {      //  decloration 5
+
+//      function myHobbies() {
+//        console.log('Coding and Playing football');
+//      }
+ 
+//       function myBankBalance() {
+//         console.log('Hisobingizda mablag yetarli emas');
+//       }
+ 
+//      myHobbies();
+//      myBankBalance();
+// }
+
+// myWorld();
+
+
+
+
+
+// let x = function (num) {   // expression 1
+//     return num * 85 
+// };
+
+// console.log(x(225));
+
+
+
+// let x = function (num) {   //  expression 2
+//     return num * num 
+// };
+
+// console.log(x(15));
+
+// let y = x(8);
+// console.log(y);
+
+
+// let fullName = function (firstName, lastName) {      //  expression 3
+//     return `MY full Name is ${firstName} ${lastName}`
+// }
+
+// console.log(fullName('Marcus', 'Rashford'));
+
+
+
+// let today = function(day){   //  expression 4
+//     if (day === 'Monday'){
+//       return true;
+//     } else{
+//       return false;
+//     }
+//   }
+ 
+// console.log(today("Monday"));
+
+
+
+// function sumA(a, b) {   //  expression 5
+//     return a + b;
+// }
+
+// function sumB(a, b) {
+//     return a + b;
+// }
+
+// console.log(sumA(5, 852));  
+// console.log(sumB(-58, 26));
+
+  
+    
+
+
+
+
